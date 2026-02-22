d000baebcf18f31ba64e31673b5a421cc474ffb5
Daily Imperative User <you@example.com>
2026-02-22 15:30:10 +0530

    Edit for vercel

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -15,7 +15,7 @@ export async function updateSession(request: NextRequest) {
                     return request.cookies.getAll()
                 },
                 setAll(cookiesToSet) {
-                    cookiesToSet.forEach(({ name, value, options }) => request.cookies.set({ name, value, ...options }))
+                    cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
                     supabaseResponse = NextResponse.next({
                         request,
                     })
@@ -24,32 +24,31 @@ export async function updateSession(request: NextRequest) {
                     )
                 },
             },
-        },
         }
     )
 
-// refreshing the auth token
-const {
-    data: { user },
-} = await supabase.auth.getUser()
+    // refreshing the auth token
+    const {
+        data: { user },
+    } = await supabase.auth.getUser()
 
-if (
-    !user &&
-    !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
-) {
-    // no user, potentially respond by redirecting the user to the login page
-    const url = request.nextUrl.clone()
-    url.pathname = "/login"
-    return NextResponse.redirect(url)
-}
+    if (
+        !user &&
+        !request.nextUrl.pathname.startsWith("/login") &&
+        !request.nextUrl.pathname.startsWith("/auth")
+    ) {
+        // no user, potentially respond by redirecting the user to the login page
+        const url = request.nextUrl.clone()
+        url.pathname = "/login"
+        return NextResponse.redirect(url)
+    }
 
-// If user is logged in, and tries to visit login, redirect to home
-if (user && request.nextUrl.pathname.startsWith("/login")) {
-    const url = request.nextUrl.clone()
-    url.pathname = "/"
-    return NextResponse.redirect(url)
-}
+    // If user is logged in, and tries to visit login, redirect to home
+    if (user && request.nextUrl.pathname.startsWith("/login")) {
+        const url = request.nextUrl.clone()
+        url.pathname = "/"
+        return NextResponse.redirect(url)
+    }
 
-return supabaseResponse
+    return supabaseResponse
 }
